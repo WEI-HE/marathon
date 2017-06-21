f5b39755f2fc0e2eb4f431d6db5f0c0e1025e5ae
Tim Harper <tharper@mesosphere.com>
2017-06-20 21:43:11 -0600

    Further simplify leader election by removing locks
    CuratorElectionService is already thread safe; further, it uses locks.
    By using also using locks in our election service, we were burdened
    with the complexity of escaping them in order to avoid
    deadlocks (which happened).
    
    Further, we were calling in to external objects that we do not
    control while we had said lock. While in theory this should have been
    fine, it is an additional source of hidden complexity.
    
    We can easily use AtomicBooleans to coordinate as needed. Since state
    transitions only one direction, this becomes easy and manageable.

--- src/main/scala/mesosphere/marathon/core/election/impl/CuratorElectionService.scala
+++ src/main/scala/mesosphere/marathon/core/election/impl/CuratorElectionService.scala
@@ -3,6 +3,7 @@ package core.election.impl
 
 import java.util
 import java.util.Collections
+import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.{ ExecutorService, Executors, TimeUnit }
 
 import akka.actor.{ ActorSystem, ActorRef }
@@ -10,10 +11,10 @@ import akka.event.EventStream
 import com.typesafe.scalalogging.StrictLogging
 import kamon.Kamon
 import kamon.metric.instrument.Time
+import mesosphere.marathon.core.async.ExecutionContexts
 import mesosphere.marathon.core.base._
 import mesosphere.marathon.core.election.{ ElectionEventListener, ElectionService, LocalLeadershipEvent }
 import mesosphere.marathon.metrics.{ Metrics, ServiceMetric, Timer }
-import mesosphere.marathon.util.RichLock
 import org.apache.curator.framework.api.ACLProvider
 import org.apache.curator.framework.imps.CuratorFrameworkState
 import org.apache.curator.framework.recipes.leader.{ LeaderLatch, LeaderLatchListener }
@@ -22,8 +23,6 @@ import org.apache.curator.retry.ExponentialBackoffRetry
 import org.apache.zookeeper.ZooDefs
 import org.apache.zookeeper.data.ACL
 
-import scala.async.Async
-import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.util.control.NonFatal
 
@@ -43,78 +42,67 @@ class CuratorElectionService(
   lifecycleState: LifecycleState)
     extends ElectionService with StrictLogging {
 
-  system.registerOnTermination(lock {
+  system.registerOnTermination {
     logger.info("Stopping leadership on shutdown")
     stop(exit = false)
-  })
-
-  /**
-    * This lock is used to linearize methods with side effects.
-    * Performance impact doesn't matter here at all, methods of this class are called infrequently.
-    */
-  private val lock = RichLock()
+  }
 
   private val threadExecutor: ExecutorService = Executors.newSingleThreadExecutor()
   /** We re-use the single thread executor here because some methods of this class might get blocked for a long time. */
-  private implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(threadExecutor)
+  // private implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(threadExecutor)
 
+  private val leadershipOffered = new AtomicBoolean(false)
+  private val leaderLatchStarted = new AtomicBoolean(false)
   private[this] var electionListener: Option[ElectionEventListener] = None
-  private[this] var isCurrentlyLeading: Boolean = false
+  @volatile private var leaderLatch: Option[LeaderLatch] = None
+  @volatile private[this] var isCurrentlyLeading: Boolean = false
 
   private lazy val client: CuratorFramework = CuratorElectionService.newCuratorConnection(config)
 
-  private var leaderLatch: Option[LeaderLatch] = None
-
-  override def isLeader: Boolean = lock {
+  override def isLeader: Boolean =
     isCurrentlyLeading
-  }
 
   override def localHostPort: String = hostPort
 
   override def leaderHostPort: Option[String] = leaderHostPortMetric.blocking {
-    lock {
-      if (client.getState == CuratorFrameworkState.STOPPED) None
-      else {
-        try {
-          leaderLatch.flatMap { latch =>
-            val participant = latch.getLeader
-            if (participant.isLeader) Some(participant.getId) else None
-          }
-        } catch {
-          case NonFatal(ex) =>
-            logger.error("Error while getting current leader", ex)
-            None
+    if (client.getState == CuratorFrameworkState.STOPPED) None
+    else {
+      try {
+        leaderLatch.flatMap { latch =>
+          val participant = latch.getLeader
+          if (participant.isLeader) Some(participant.getId) else None
         }
+      } catch {
+        case NonFatal(ex) =>
+          logger.error("Error while getting current leader", ex)
+          None
       }
     }
   }
 
-  override def offerLeadership(electionListener: ElectionEventListener): Unit = lock {
+  override def offerLeadership(electionListener: ElectionEventListener): Unit = {
     logger.info("offered leadership")
-    this.electionListener match {
-      case Some(_) =>
-        logger.error("Error! Leadership already offered! Exiting now")
-        stop(exit = true)
-
-      case None if (!lifecycleState.isRunning) =>
-        logger.info("Not trying to get leadership since Marathon is shutting down")
-
-      case None =>
+    if (leadershipOffered.compareAndSet(false, true)) {
+      if (lifecycleState.isRunning) {
         this.electionListener = Some(electionListener)
-        Async.async {
-          logger.info("Going to acquire leadership")
-          try {
-            startLeaderLatch()
-          } catch {
-            case NonFatal(ex) =>
-              logger.error("Fatal error while acquiring leadership. Exiting now", ex)
-              stop(exit = true)
-          }
+        logger.info("Going to acquire leadership")
+        try {
+          startLeaderLatch()
+        } catch {
+          case NonFatal(ex) =>
+            logger.error("Fatal error while acquiring leadership. Exiting now", ex)
+            stop(exit = true)
         }
+      } else {
+        logger.info("Not trying to get leadership since Marathon is shutting down")
+      }
+    } else {
+      logger.error("Error! Leadership already offered! Exiting now")
+      stop(exit = true)
     }
   }
 
-  private def startLeaderLatch(): Unit = lock {
+  private def startLeaderLatch(): Unit = if (leaderLatchStarted.compareAndSet(false, true)) {
     require(leaderLatch.isEmpty, "leaderLatch is not empty")
 
     val latch = new LeaderLatch(
@@ -122,33 +110,38 @@ class CuratorElectionService(
     latch.addListener(LeaderChangeListener, threadExecutor)
     latch.start()
     leaderLatch = Some(latch)
+  } else {
+    logger.info("Not starting leader latch; either duplicate call or shutting down")
   }
 
-  private def leadershipAcquired(): Unit = Async.async {
-    lock {
-      electionListener match {
-        case Some(c) =>
-          try {
-            startLeadership(c)
-            isCurrentlyLeading = true
-          } catch {
-            case NonFatal(ex) =>
-              logger.error(s"Fatal error while starting leadership of $electionListener. Exiting now", ex)
-              stop(exit = true)
-          }
-        case _ =>
-          logger.error(s"Acquired leadership (electionListener: $electionListener, isLeader: ${isLeader}). Exiting now")
+  private[this] def leadershipAcquired(): Unit = electionListener match {
+    case Some(c) =>
+      try {
+        isCurrentlyLeading = true
+        logger.info(s"Starting $electionListener's leadership")
+        c.startLeadership()
+        logger.info(s"Started $electionListener's leadership")
+        // Note - it is vitally important that this come after isCurrentlyLeading = true in order to guarantee state
+        // receipt by subscriber
+        eventStream.publish(LocalLeadershipEvent.ElectedAsLeader)
+        logger.info(s"$electionListener has started")
+        startMetrics()
+      } catch {
+        case NonFatal(ex) =>
+          logger.error(s"Fatal error while starting leadership of $electionListener. Exiting now", ex)
           stop(exit = true)
       }
-    }
+    case _ =>
+      logger.error("Illegal state! Acquired leadership but no listener. Exiting now")
+      stop(exit = true)
   }
 
-  override def abdicateLeadership(): Unit = lock {
+  override def abdicateLeadership(): Unit = {
     logger.info("Abdicating leadership")
     stop(exit = true)
   }
 
-  private def stop(exit: Boolean): Unit = lock {
+  private def stop(exit: Boolean): Unit = {
     logger.info("Stopping the election service")
     try {
       stopLeadership()
@@ -158,6 +151,7 @@ class CuratorElectionService(
     } finally {
       isCurrentlyLeading = false
       if (exit) {
+        import ExecutionContexts.global
         system.scheduler.scheduleOnce(500.milliseconds) {
           Runtime.getRuntime.asyncExit()
         }
@@ -165,16 +159,7 @@ class CuratorElectionService(
     }
   }
 
-  private def startLeadership(electionListener: ElectionEventListener): Unit = lock {
-    logger.info(s"Starting $electionListener's leadership")
-    electionListener.startLeadership()
-    logger.info(s"Started $electionListener's leadership")
-    eventStream.publish(LocalLeadershipEvent.ElectedAsLeader)
-    logger.info(s"$electionListener has started")
-    startMetrics()
-  }
-
-  private def stopLeadership(): Unit = lock {
+  private def stopLeadership(): Unit = {
     stopMetrics()
     if (isLeader) {
       electionListener.foreach { l =>
@@ -185,15 +170,19 @@ class CuratorElectionService(
       eventStream.publish(LocalLeadershipEvent.Standby)
     }
 
-    leaderLatch.foreach { latch =>
-      try {
-        if (client.getState != CuratorFrameworkState.STOPPED) latch.close()
-      } catch {
-        case NonFatal(ex) =>
-          logger.error("Could not close leader latch", ex)
+    if (leaderLatchStarted.compareAndSet(false, true)) {
+      () // do nothing; we just prevented it from ever starting
+    } else {
+      leaderLatch.foreach { latch =>
+        try {
+          if (client.getState != CuratorFrameworkState.STOPPED) latch.close()
+        } catch {
+          case NonFatal(ex) =>
+            logger.error("Could not close leader latch", ex)
+        }
       }
+      leaderLatch = None
     }
-    leaderLatch = None
   }
 
   /**
@@ -203,43 +192,38 @@ class CuratorElectionService(
     * for LeaderLatch.setLeadership
     */
   private object LeaderChangeListener extends LeaderLatchListener {
-    override def notLeader(): Unit = Async.async {
+    override def notLeader(): Unit = {
       logger.info(s"Leader defeated. New leader: ${leaderHostPort.getOrElse("-")}")
       stop(exit = true)
     }
 
-    override def isLeader(): Unit = Async.async {
+    override def isLeader(): Unit = {
       logger.info("Leader elected")
       leadershipAcquired()
     }
   }
 
-  private var metricsStarted = false
+  private val metricsStarted = new AtomicBoolean(false)
   private val leaderDurationMetric = "service.mesosphere.marathon.leaderDuration"
   private val leaderHostPortMetric: Timer = Metrics.timer(ServiceMetric, getClass, "current-leader-host-port")
 
-  protected def startMetrics(): Unit = lock {
-    if (!metricsStarted) {
+  protected def startMetrics(): Unit =
+    if (metricsStarted.compareAndSet(false, true)) {
       val startedAt = System.currentTimeMillis()
       Kamon.metrics.gauge(leaderDurationMetric, Time.Milliseconds)(System.currentTimeMillis() - startedAt)
-      metricsStarted = true
     }
-  }
 
-  protected def stopMetrics(): Unit = lock {
-    if (metricsStarted) {
+  protected def stopMetrics(): Unit =
+    if (metricsStarted.compareAndSet(true, false))
       Kamon.metrics.removeGauge(leaderDurationMetric)
-      metricsStarted = false
-    }
-  }
 
-  def subscribe(subscriber: ActorRef): Unit = lock {
+  def subscribe(subscriber: ActorRef): Unit = {
     eventStream.subscribe(subscriber, classOf[LocalLeadershipEvent])
-    val currentState = if (isLeader) LocalLeadershipEvent.ElectedAsLeader else LocalLeadershipEvent.Standby
+    val currentState = if (isCurrentlyLeading) LocalLeadershipEvent.ElectedAsLeader else LocalLeadershipEvent.Standby
     subscriber ! currentState
   }
 
-  def unsubscribe(subscriber: ActorRef): Unit = lock {
+  def unsubscribe(subscriber: ActorRef): Unit = {
     eventStream.unsubscribe(subscriber, classOf[LocalLeadershipEvent])
   }
 }
